72d07619c4e1088fe8c33cfc44f052c3054b1b01
Jose Carlos de Jesus Junior <juninho_web@hotmail.com>
2019-02-09 10:29:17 -0200

    Initial commit

--- Logic/SimpleBotUser.cs
+++ Logic/SimpleBotUser.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace SimpleBot.Logic
@@ -9,8 +12,34 @@ namespace SimpleBot.Logic
     {
         public string Reply(SimpleMessage message)
         {
+            SaveMessage(message.Text);
+
             return $"{message.User} disse '{message.Text}";
         }
 
+        private async Task SaveMessage(string message)
+        {
+            var conn = "mongodb://localhost:27017";
+
+            var client = new MongoClient(conn);
+
+            var db = client.GetDatabase("demo");
+            var col = db.GetCollection<BsonDocument>("messages");
+
+            var document = new BsonDocument
+            {
+              {"message", message},
+            };
+
+            await col.InsertOneAsync(document);
+
+            client = null;
+            db     = null;
+        }
+    }
+
+    public class Message: BsonDocument
+    {
+        public string Text { get; set; }
     }
 }
